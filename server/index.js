eee3a64f0d5e794b6415d49fada81416e81d75b7
LyghtJr <allenrai1530@gmail.com>
2021-11-06 15:09:34 +0530

    Connect MongoDB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const connectDB = require('./db/connect');
 const app = express();
+require("dotenv").config();
 
 app.use(express.json());
 
 const port = 5000;
 
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URI);
 app.listen(port,()=>{
     console.log(`server Started on Port ${port}`);
 });
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
+
+start();
